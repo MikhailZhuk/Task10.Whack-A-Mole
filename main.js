87507a2218d98725f014e9d12ddee3357d73424e
Mikhail Zhuk <miha-44@mail.ru>
2020-12-08 13:46:05 +0300

    feat: add speed level

--- main.js
+++ main.js
@@ -4,16 +4,59 @@ const scoreBoard = document.querySelector('.score');
 let lastHole;
 let timeUp = false;
 let score = 0;
+const levelEasy = document.querySelector('.easy');
+const levelNormal = document.querySelector('.normal');
+const levelHard = document.querySelector('.hard');
+const levelExteme = document.querySelector('.extreme');
 
 function randomTime(min, max) {
   return Math.round(Math.random()) * (max - min) + min;
 }
 
+min=300;
+max=900;
+
+levelEasy.addEventListener('click', () => {
+  levelEasy.classList.add('focus');
+  levelNormal.classList.remove('focus');
+  levelHard.classList.remove('focus');
+  levelExteme.classList.remove('focus');
+  min=400;
+  max=1000;
+})
+
+levelNormal.addEventListener('click', () => {
+  levelNormal.classList.add('focus');
+  levelEasy.classList.remove('focus');
+  levelHard.classList.remove('focus');
+  levelExteme.classList.remove('focus');
+  min=300;
+  max=900;
+})
+
+levelHard.addEventListener('click', () => {
+  levelHard.classList.add('focus');
+  levelEasy.classList.remove('focus');
+  levelNormal.classList.remove('focus');
+  levelExteme.classList.remove('focus');
+  min=200;
+  max=700;
+})
+
+levelExteme.addEventListener('click', () => {
+  levelExteme.classList.add('focus');
+  levelEasy.classList.remove('focus');
+  levelNormal.classList.remove('focus');
+  levelHard.classList.remove('focus');
+  min=200;
+  max=500;
+})
+
 function randomHole(holes) {
   const idx = Math.floor( Math.random() * holes.length);
   const hole = holes[idx];
   if( hole === lastHole) {
-    console.log('ah nah');
+    console.log('ah sorry');
     return randomHole(holes);
   }
   lastHole = hole;
@@ -21,7 +64,8 @@ function randomHole(holes) {
 }
 
 function peep() {
-  const time = randomTime( 200, 700);
+  const time = randomTime(min, max);
+  console.log(time);
   const hole = randomHole(holes);
   hole.classList.add('up');
   setTimeout(() => {
@@ -35,7 +79,7 @@ function startGame() {
   timeUp = false;
   score = 0;
   peep();
-  setTimeout(() => timeUp = true, 10000)
+  setTimeout(() => timeUp = true, 10000);
 }
 
 function bonk(e) {
@@ -47,4 +91,3 @@ function bonk(e) {
 }
 
 moles.forEach( mole => mole.addEventListener('click', bonk));
-
